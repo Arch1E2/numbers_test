822cc3dea8403c727583a142e3635279c386282f
arch1 <arch1e2.work@gmail.com>
2022-06-25 18:46:09 +0400

    float fix

--- numbers_test/app/views.py
+++ numbers_test/app/views.py
@@ -9,9 +9,7 @@ def orders(request):
     result = []
     for order in orders:
         total_cost = [str(order.total_cost_in_rubles), str(order.total_cost_in_rubles_after_comma)]
-        print(total_cost)
         order_total_cost_in_rubles = '.'.join(total_cost)
-        print(order_total_cost_in_rubles)
         result.append({
             'index_in_table': order.index_in_table,
             'order_id': order.order_id,
